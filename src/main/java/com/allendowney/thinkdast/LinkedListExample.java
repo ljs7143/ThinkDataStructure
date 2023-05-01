330be8314eb733ceb71a79a7efd9cdeb32fca502
ljs7143 <junseo27643779@gmail.com>
2023-05-02 00:50:04 +0900

    linkedList(study)

--- src/main/java/com/allendowney/thinkdast/LinkedListExample.java
+++ src/main/java/com/allendowney/thinkdast/LinkedListExample.java
@@ -4,7 +4,8 @@ package com.allendowney.thinkdast;
  * @author downey
  *
  */
-public class LinkedListExample {	
+public class
+LinkedListExample {
 	
 	/**
 	 * @param args
@@ -13,7 +14,10 @@ public class LinkedListExample {
 		ListNode node1 = new ListNode(1);
 		ListNode node2 = new ListNode(2);
 		ListNode node3 = new ListNode(3);
-		
+
+
+		//연결하는 과정
+		//관례상 마지막 노드의 next는 null로 처리함
 		node1.next = node2;
 		node2.next = node3;
 		node3.next = null;
--- src/main/java/com/allendowney/thinkdast/MyLinkedList.java
+++ src/main/java/com/allendowney/thinkdast/MyLinkedList.java
@@ -40,8 +40,12 @@ public class MyLinkedList<E> implements List<E> {
 		}
 	}
 
-	private int size;            // keeps track of the number of elements
-	private Node head;           // reference to the first node
+//size 변수를 명시적으로 저장하면 상수 시간으로 size 메서드를 구현할 수 있다
+// 그렇지 않으면 리스트를 순회하여 요소 개수를 세는 선형 시간이 필요하다
+// ---> 새로운 노드 추가 시 head변수를 새로운 노드로 업데이트하고 size변수 1증가 시키면 됨
+	// 그러나 size변수 저장하지 않으면 리스트 크기를 계산하기 위해 전체 순회해야함 --> O(n)소요 --> 선형
+	private int size;    //요소의 개수 추적        // keeps track of the number of elements
+	private Node head;    //첫 번째 노드에 대한 참조       // reference to the first node
 
 	/**
 	 *
@@ -73,7 +77,7 @@ public class MyLinkedList<E> implements List<E> {
 		} else {
 			Node node = head;
 			// loop until the last node
-			for ( ; node.next != null; node = node.next) {}
+			for ( ; node.next != null; node = node.next) {}    //노드의 next포인터가 null이 아니면 node는 next노드로 변경. 즉 끝까지 순환하여 마지막에 새로운 노드를 추가함
 			node.next = new Node(element);
 		}
 		size++;
@@ -81,8 +85,20 @@ public class MyLinkedList<E> implements List<E> {
 	}
 
 	@Override
-	public void add(int index, E element) {
+	public void add(int index, E element) {  //선형메서드
+
 		//TODO: FILL THIS IN!
+
+		if( index == 0){
+			head = new Node(element, head);
+		}else{
+			Node node = getNode(index-1);
+			node.next = new Node(element, node.next);
+		}
+		size++;
+
+
+
 	}
 
 	@Override
@@ -101,7 +117,7 @@ public class MyLinkedList<E> implements List<E> {
 
 	@Override
 	public void clear() {
-		head = null;
+		head = null;  // 첫번째 Node에 대한 참조를 제거함
 		size = 0;
 	}
 
@@ -130,10 +146,14 @@ public class MyLinkedList<E> implements List<E> {
 	 * @param index
 	 * @return
 	 */
-	private Node getNode(int index) {
+
+	private Node getNode(int index) {   //선형메서드
+		//예외처리
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException();
 		}
+
+
 		Node node = head;
 		for (int i=0; i<index; i++) {
 			node = node.next;
@@ -144,6 +164,16 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		//TODO: FILL THIS IN!
+		//실행시간은 O(n)
+
+
+		Node node = head; //head의 사본을 얻음
+		for(int i=0; i<size-1; i++){
+			if(equals(target, node.data)){
+				return i;
+			}
+			node = node.next;
+		}
 		return -1;
 	}
 
@@ -209,7 +239,22 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public E remove(int index) {
 		//TODO: FILL THIS IN!
-		return null;
+
+		E element = get(index);
+		if(index ==0){
+			//head가 사라지기 때문에 그 다음꺼를 헤드로 변경해줌
+			head = head.next;
+		}else{
+			Node node = getNode(index-1);
+			node.next = node.next.next;
+
+			//ex) 1,2,3,4에서 2를 삭제하는 경우
+			//node는 1이 됨, 1의 다음은 3이 됨
+			//1,3,4가 됨
+
+		}
+		size--;
+		return element; //이래야 나중에 혹시라도 무엇을 삭제했는지 정보가 필요하게 되었을 때 문제가 발생하지 않음
 	}
 
 	@Override
